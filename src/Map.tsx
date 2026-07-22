3d37bb1df5ed2157f5c3f18012e220a7f0aab396
oleshka12345 <oleshka@fishpoint.com>
2026-07-22 13:05:16 +0300

    Fix leaflet map

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,28 +1,92 @@
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 
 
-function Map() {
+type Catch = {
+  fishName: string
+  weight: string
+  place: string
+  date: string
+  photo: string
+  location: string
+}
+
+
+type Props = {
+  catches: Catch[]
+}
+
+
+function Map({ catches }: Props) {
 
   return (
 
-    <MapContainer
-      center={[49.9935, 36.2304]}
-      zoom={12}
-      style={{
-        height: '400px',
-        width: '100%'
-      }}
-    >
+    <div style={{ height: '400px', width: '100%' }}>
 
-      <TileLayer
-        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+      <MapContainer
+        center={[49.9935, 36.2304]}
+        zoom={12}
+        style={{
+          height: '400px',
+          width: '100%'
+        }}
+      >
 
-    </MapContainer>
+        <TileLayer
+          attribution='&copy; OpenStreetMap contributors'
+          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+
+        {catches.map((item, index) => {
+
+          if (!item.location) return null
+
+
+          const position = item.location
+            .split(',')
+            .map(Number) as [number, number]
+
+
+          return (
+
+            <Marker
+              key={index}
+              position={position}
+            >
+
+              <Popup>
+
+                <b>
+                  🐟 {item.fishName}
+                </b>
+
+                <br />
+
+                ⚖️ {item.weight} кг
+
+                <br />
+
+                📍 {item.place}
+
+                <br />
+
+                📅 {item.date}
+
+              </Popup>
+
+            </Marker>
+
+          )
+
+        })}
+
+
+      </MapContainer>
+
+    </div>
 
   )
-
 }
 
 
