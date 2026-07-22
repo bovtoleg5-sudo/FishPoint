eca9b1b4f8217234f38bcd48954340d269f3bc54
oleshka12345 <oleshka@fishpoint.com>
2026-07-22 12:53:08 +0300

    Fix map build

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,72 +1,28 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 
 
-function Map({ catches }: any) {
-
-  const position: [number, number] = [
-    49.9935,
-    36.2304
-  ]
-
+function Map() {
 
   return (
 
     <MapContainer
-      center={position}
-      zoom={13}
-      style={{ height: '400px', width: '100%' }}
+      center={[49.9935, 36.2304]}
+      zoom={12}
+      style={{
+        height: '400px',
+        width: '100%'
+      }}
     >
-
 
       <TileLayer
         url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
-
-      {catches.map((item: any, index: number) => {
-
-        if (!item.location) return null
-
-        const coords = item.location.split(',')
-
-
-        return (
-
-          <Marker
-            key={index}
-            position={[
-              Number(coords[0]),
-              Number(coords[1])
-            ]}
-          >
-
-            <Popup>
-
-              🎣 {item.fishName}
-              <br />
-
-              ⚖️ {item.weight} кг
-              <br />
-
-              📍 {item.place}
-              <br />
-
-              📅 {item.date}
-
-            </Popup>
-
-
-          </Marker>
-
-        )
-
-      })}
-
-
     </MapContainer>
 
   )
+
 }
 
 
